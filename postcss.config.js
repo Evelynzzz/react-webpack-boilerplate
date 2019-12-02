17695d4d0d4b6d6628a0d6a6a567a2470499de59
Evelynzzz <15104026362@163.com>
2019-12-02 18:52:22 +0800

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   plugins: [
-       require('autoprefixer')(),
+       require('autoprefixer'),
        env === 'production' ? require('cssnano') : null
     ]
 })
