b2803b0dde1806ca4241de577e8aa089cef964d4
Evelynzzz <15104026362@163.com>
2019-12-17 19:29:06 +0800

    Try use hook useContext

--- html/components/Home.js
+++ html/components/Home.js
@@ -1,8 +1,10 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
+import {ThemeContext} from './ThemeContext'
 
 export default function Home(props){
   const [name, setName]= useState("Evelyn")
   const [surname, setSurname]= useState("ZHONG")
+  const {theme} = useContext(ThemeContext)
 
   function handleNameChange(e){
     setName(e.target.value)
@@ -20,23 +22,25 @@ export default function Home(props){
     setSurname("")
   }
 
-
   return (
     <div>
-        {<p>Hello {name} {surname}!</p>}
-        <input type="text"
+      {<p>Hello {name} {surname}!</p>}
+      <input type="text"
         placeholder="What's your name?"
         onChange={handleNameChange.bind(this)}
         value={name}
-        />
-        <button onClick={handleClearName.bind(this)}>Clear</button>
-        <br/>
-        <input type="text"
+      />
+      <button onClick={handleClearName.bind(this)}>Clear</button>
+      <br/>
+      <input type="text"
         placeholder="What's your surname?"
         onChange={handleSurnameChange.bind(this)}
         value={surname}
-        />
-        <button onClick={handleClearSurname.bind(this)}>Clear</button>
+      />
+      <button onClick={handleClearSurname.bind(this)}>Clear</button>
+      <br/>
+      <br/>
+      <p>Now your are using {theme} theme :)</p>
     </div>
   )
 }
